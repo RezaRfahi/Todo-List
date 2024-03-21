a0382d5e6f54b7e5eca0cf1af7a6bd7b40192cdc
RezaRfahi <refahireza82@gmail.com>
2024-03-22 01:05:52 +0330

    Edit index

--- views/layout.php
+++ views/layout.php
@@ -1,9 +1,12 @@
+<?php
+
+echo '
 <!DOCTYPE html>
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
   <title>CodePen - Task manager UI</title>
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
 
 </head>
 <body>
@@ -71,7 +74,9 @@
   </div>
 </div>
 <!-- partial -->
-  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script  src="../assets/js/script.js"></script>
 
 </body>
 </html>
+';
